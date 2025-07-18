271e5fb83f29c121845bcdc59af65bcb3df6c408
anjalitarate123 <anjalitarate3@gmail.com>
2025-07-18 19:01:54 +0530

    Add files via upload

---
+++ c_programing/Assignment 1/Q1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int no=12;
+if(no%12==2)
+{
+printf(" the number is even");
+}
+else
+printf("the number is odd");
+}
---
+++ c_programing/Assignment 1/Q4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	char ch = 'a';
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+	{
+		printf("this is vowel");
+	}
+else
+{
+	if (ch>='a'&&ch<'z')
+{
+printf("this is consument");
+}
+if(ch>='0'&&ch<='9')
+{
+	printf("this is special symbol");
+   }
+}
+}
+  
